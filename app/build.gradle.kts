c23acd2600731e1d47a024590d8f9eef2935d3e0
jonathanwijayaa <jonathan.wijaya@ti.ukdw.ac.id>
2025-03-24 21:14:43 +0700

    ini home page + item note
    nanti nampilin per 2 2 pake grid manager kayaknya

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -41,8 +41,10 @@ dependencies {
     implementation(libs.androidx.appcompat)
     implementation(libs.material)
     implementation(libs.androidx.activity)
+    implementation(libs.androidx.gridlayout)
     implementation(libs.androidx.constraintlayout)
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
+
 }
