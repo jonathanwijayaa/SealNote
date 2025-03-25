edee735aeb9444f3fb6afe21dcdba36047fd51a3
jonathanwijayaa <jonathan.wijaya@ti.ukdw.ac.id>
2025-03-26 01:43:09 +0700

    update imut

---
+++ app/src/main/java/com/example/sealnote/ProfileFragment.kt
@@ -0,0 +1,26 @@
+package com.example.sealnote
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import androidx.navigation.fragment.findNavController
+
+class ProfileFragment : Fragment() {
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        return inflater.inflate(R.layout.profile_page, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        val toolbar = view.findViewById<androidx.appcompat.widget.Toolbar>(R.id.toolbar)
+        toolbar.setNavigationOnClickListener {
+            findNavController().navigateUp()
+        }
+    }
+}
